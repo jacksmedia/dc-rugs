b6a9467dea27f226fdf3083b338f6bca6e28aa6a
jacksmedia <alex@j4cks.com>
2024-01-23 21:22:26 -0800

    🎨 improves Landing page styling

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -3,13 +3,14 @@ import Button from 'react-bootstrap/Button';
 
 export const Landing = () => {
     return (
-        <div className='center-panel bg-transparent'>
-            <h1 className='white-text'>Welcome to Rug's Royalties</h1>
+        <div className='center-panel bg-transparent text-end'>
+            <h1 className='white-text three-rem'>Welcome to Rug's Royalties</h1>
             <a href={RouteNamesEnum.login}>
-                <Button className='center '>
-                    Login to Rug Your NFTs
+                <Button className='center red-button'>
+                    Login
                 </Button>
             </a>
+            <h1 className='white-text'>to Rug Your NFTs</h1>
         </div>
     );
 };
